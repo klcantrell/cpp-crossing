12dfac48bcb536d1266847138cf567f2f1b1ea76
klcantrell <cantrellkalalau@gmail.com>
2021-03-27 10:36:03 -0400

    basics_frankm: add language basics
    Add .gitignore for embedded_oo_gbati

---
+++ basics_frankm/LanguageBasics/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    // BASIC I/O
+    int roomWidth {0};
+    cout << "Enter the width of the room: ";
+    cin >> roomWidth;
+    
+    int roomLength {0};
+    cout << "Enter the length of the room: ";
+    cin >> roomLength;
+    
+    cout << "The area of the room is " << roomWidth * roomLength << " square feet." << endl;
+    
+    
+    // PRIMITIVE TYPES
+    
+    
+    return 0;
+}
