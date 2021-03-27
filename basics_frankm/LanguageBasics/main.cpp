b7a7d4be1c62e2cff986a6fb53cb4be50c96382c
klcantrell <cantrellkalalau@gmail.com>
2021-03-27 11:42:46 -0400

    basics_frankm: update main.cpp

--- basics_frankm/LanguageBasics/main.cpp
+++ basics_frankm/LanguageBasics/main.cpp
@@ -4,21 +4,20 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int main() {
+int main()
+{
     // BASIC I/O
-    int roomWidth {0};
+    int roomWidth{0};
     cout << "Enter the width of the room: ";
     cin >> roomWidth;
-    
-    int roomLength {0};
+
+    int roomLength{0};
     cout << "Enter the length of the room: ";
     cin >> roomLength;
-    
+
     cout << "The area of the room is " << roomWidth * roomLength << " square feet." << endl;
-    
-    
+
     // PRIMITIVE TYPES
-    
-    
+
     return 0;
 }
