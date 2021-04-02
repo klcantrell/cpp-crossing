430a9d91e9b4f3f927064fed948c93148c4c3418
klcantrell <cantrellkalalau@gmail.com>
2021-04-01 20:25:56 -0400

    basics_frankm/Language Basics: finish constants

--- basics_frankm/LanguageBasics/constants.cpp
+++ basics_frankm/LanguageBasics/constants.cpp
@@ -8,21 +8,29 @@ void constants()
 {
   cout << "Hello, welcome to Frank's Carpet Cleaning Service" << endl;
   cout << endl;
-  cout << "How many rooms would you like cleaned? ";
+  cout << "How many small rooms would you like cleaned? ";
 
-  int number_of_rooms{0};
-  cin >> number_of_rooms;
+  int number_of_small_rooms{0};
+  cin >> number_of_small_rooms;
 
-  const double price_per_room{30};
+  cout << "How many large rooms would you like cleaned? ";
+
+  int number_of_large_rooms{0};
+  cin >> number_of_large_rooms;
+
+  const double price_per_small_room{25};
+  const double price_per_large_room{35};
   const double sales_tax{0.06};
   const int estimate_expiry{30};
 
   cout << endl;
   cout << "Estimate for carpet cleaning service" << endl;
-  cout << "Number of rooms: " << number_of_rooms << endl;
-  cout << "Price per room: $" << price_per_room << endl;
+  cout << "Number of small rooms: " << number_of_small_rooms << endl;
+  cout << "Price per small room: $" << price_per_small_room << endl;
+  cout << "Number of large rooms: " << number_of_large_rooms << endl;
+  cout << "Price per large room: $" << price_per_large_room << endl;
 
-  const double room_subtotal = price_per_room * number_of_rooms;
+  const double room_subtotal = price_per_small_room * number_of_small_rooms + price_per_large_room * number_of_large_rooms;
   const double calculated_tax = room_subtotal * sales_tax;
 
   cout << "Cost: $" << room_subtotal << endl;
