e2850fdfade19581a956eaf742fe81aaa904da6d
Kal Cantrell <klcantrell@sep.com>
2021-11-03 19:23:47 -0400

    basics_frankm/LanguageBasics: add example for function overloading

--- basics_frankm/LanguageBasics/functions.cpp
+++ basics_frankm/LanguageBasics/functions.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+
+using std::string;
 
 using std::cin;
 using std::cout;
@@ -8,6 +11,11 @@ using std::endl;
 void scale_number(int &);
 void scale_number();
 
+void display(int = 10);
+// void display(double = 10.2); // otherwise more than one overload would match
+void display(double);
+void display(string);
+
 void functions()
 {
     // pass by reference
@@ -15,6 +23,18 @@ void functions()
     scale_number(my_num);
     scale_number();
     cout << my_num << endl; // will be 100
+
+    display(2);
+    display('2'); // is promoted to the int overload
+    display(123.5);
+    display(123.4F); // is promoted to the double overload
+
+    string name{"Kal"};
+    display(name);
+    display(name.c_str()); // is promoted to C++ style string
+    display("Kal");        // is promoted to C++ style string
+
+    display();
 }
 
 void scale_number(int &num)
@@ -28,4 +48,19 @@ void scale_number(int &num)
 void scale_number()
 {
     cout << "You didn't pass any number argument!" << endl;
+}
+
+void display(int num)
+{
+    cout << num << endl;
+}
+
+void display(double num)
+{
+    cout << num << endl;
+}
+
+void display(string str)
+{
+    cout << str << endl;
 }
